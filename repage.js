a3cc36fdd3636ba858d51e61ae59963093e86dba
John Victor <johnpazzz01@gmail.com>
2024-05-18 20:22:43 -0300

    FIX: New class index.hmtl and new archive repage.js

--- repage.js
+++ repage.js
@@ -1,9 +1,11 @@
 // redirecionamento.js
 
-// Função para redirecionar para uma nova URL
+// Função para redirecionar para uma nova URL após 5 segundos
 function redirecionar() {
     // Corrigindo o caminho para o arquivo Main.html
-    window.location.href = '/Login/Main.html';
+    setTimeout(function() {
+        window.location.href = '/Login/Main.html';
+    }, 5000); // Tempo em milissegundos (5 segundos)
 }
 
 // Chamar a função de redirecionamento imediatamente quando o script for carregado
